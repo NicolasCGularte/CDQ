1aca2765d969e1a55739394a668530a77808d73b
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2023-02-15 18:24:16 -0300

    Add files via upload

--- teste9GUI.py
+++ teste9GUI.py
@@ -1,99 +1,83 @@
+import tkinter as tk
 import datetime
 import webbrowser
-import tkinter as tk
-from tkinter import messagebox
 
-class ConfirmacaoPresencaGUI:
+class GUI:
+    def __init__(self, playlist_url):
+        self.confirmacao_presenca = ConfirmacaoPresenca()
+        self.playlist = PlaylistSpotify(playlist_url)
 
-    def __init__(self, master):
-        self.master = master
-        master.title("Confirmação de Presença")
+        # Cria a janela principal
+        self.janela = tk.Tk()
+        self.janela.title("Confirmação de Presença")
 
-        self.confirmacoes = {}
+        # Cria o campo de texto para a data de confirmação
+        tk.Label(self.janela, text="Data de confirmação (dd/mm/aaaa):").grid(row=0, column=0)
+        self.data_confirmacao_entry = tk.Entry(self.janela)
+        self.data_confirmacao_entry.grid(row=0, column=1)
 
-        # Label e Entry para a data de confirmação
-        self.data_confirmacao_label = tk.Label(master, text="Data de confirmação (dd/mm/aaaa):")
-        self.data_confirmacao_label.grid(row=0, column=0, padx=10, pady=10)
-        self.data_confirmacao_entry = tk.Entry(master)
-        self.data_confirmacao_entry.grid(row=0, column=1, padx=10, pady=10)
+        # Cria o campo de texto para o nome
+        tk.Label(self.janela, text="Nome:").grid(row=1, column=0)
+        self.nome_entry = tk.Entry(self.janela)
+        self.nome_entry.grid(row=1, column=1)
 
-        # Botão para coletar as confirmações
-        self.coletar_button = tk.Button(master, text="Coletar Confirmações", command=self.coletar_confirmacoes)
-        self.coletar_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        # Cria o campo de texto para a confirmação de presença
+        tk.Label(self.janela, text="Confirmação de presença (sim/não):").grid(row=2, column=0)
+        self.confirmacao_entry = tk.Entry(self.janela)
+        self.confirmacao_entry.grid(row=2, column=1)
 
-        # Label para exibir as confirmações
-        self.confirmacoes_label = tk.Label(master, text="")
-        self.confirmacoes_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+        # Cria o botão para adicionar uma nova confirmação de presença
+        tk.Button(self.janela, text="Adicionar", command=self.adicionar_confirmacao).grid(row=3, column=0)
 
-        # Botão para efetuar o pagamento
-        self.efetuar_pagamento_button = tk.Button(master, text="Efetuar Pagamento", command=self.efetuar_pagamento)
-        self.efetuar_pagamento_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        # Cria o botão para efetuar o pagamento
+        tk.Button(self.janela, text="Efetuar pagamento", command=self.efetuar_pagamento).grid(row=4, column=0)
 
-        # Botão para reproduzir a playlist do Spotify
-        self.playlist_button = tk.Button(master, text="Reproduzir Playlist do Spotify", command=self.reproduzir_playlist)
-        self.playlist_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
-
-    def coletar_confirmacoes(self):
-        data_confirmacao_str = self.data_confirmacao_entry.get()
-        if not data_confirmacao_str:
-            tk.messagebox.showerror("Erro", "Por favor, informe a data de confirmação.")
-            return
-
-        try:
-            dia, mes, ano = map(int, data_confirmacao_str.split("/"))
-            data_confirmacao = datetime.date(ano, mes, dia)
-        except:
-            tk.messagebox.showerror("Erro", "Data de confirmação inválida. Por favor, informe no formato dd/mm/aaaa.")
-            return
-
-        self.confirmacoes = {}
-
-        # Janela para coletar as confirmações
-        self.coletar_window = tk.Toplevel(self.master)
-        self.coletar_window.title("Coletar Confirmações")
-
-        self.nome_label = tk.Label(self.coletar_window, text="Nome:")
-        self.nome_label.grid(row=0, column=0, padx=10, pady=10)
-        self.nome_entry = tk.Entry(self.coletar_window)
-        self.nome_entry.grid(row=0, column=1, padx=10, pady=10)
-
-        self.confirmacao_label = tk.Label(self.coletar_window, text="Confirmação:")
-        self.confirmacao_label.grid(row=1, column=0, padx=10, pady=10)
-        self.confirmacao_var = tk.StringVar()
-        self.confirmacao_var.set("Não")
-        self.confirmacao_sim_radio = tk.Radiobutton(self.coletar_window, text="Sim", variable=self.confirmacao_var, value="Sim")
-        self.confirmacao_sim_radio.grid(row=1, column=1, padx=10, pady=10)
-        self.confirmacao_nao_radio = tk.Radiobutton(self.coletar_window, text="Não", variable=self.confirmacao_var, value="Não")
-        self.confirmacao_nao_radio.grid(row=1, column=2, padx=10, pady=10)
-        self.adicionar_button = tk.Button(self.coletar_window, text="Adicionar", command=self.adicionar_confirmacao)
-        self.adicionar_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
-
-        self.confirmacoes_label = tk.Label(self.coletar_window, text="")
-        self.confirmacoes_label.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+        # Cria o botão para reproduzir a playlist no Spotify
+        tk.Button(self.janela, text="Reproduzir playlist", command=self.reproduzir_playlist).grid(row=5, column=0)
 
     def adicionar_confirmacao(self):
+        # Obtém a data de confirmação, nome e confirmação de presença digitados pelo usuário
+        data_confirmacao = datetime.datetime.strptime(self.data_confirmacao_entry.get(), "%d/%m/%Y").date()
         nome = self.nome_entry.get()
-        confirmacao = self.confirmacao_var.get()
+        confirmacao = self.confirmacao_entry.get()
 
+        # Adiciona a confirmação de presença na classe ConfirmacaoPresenca
         if nome and confirmacao:
-            self.confirmacoes[nome] = confirmacao
-            self.confirmacoes_label.config(text=str(self.confirmacoes))
-            self.nome_entry.delete(0, tk.END)
-            self.confirmacao_var.set("Não")
+            if confirmacao.lower() == "sim":
+                self.confirmacao_presenca.confirmacoes.setdefault(nome, {})[data_confirmacao] = confirmacao
+            elif confirmacao.lower() == "nao":
+                self.confirmacao_presenca.confirmacoes.setdefault(nome, {})[data_confirmacao] = confirmacao
+            else:
+                print("Confirmação de presença inválida.")
+        else:
+            print("Por favor, preencha todos os campos.")
 
     def efetuar_pagamento(self):
-        # Implementação para efetuar o pagamento
-        messagebox.showinfo("Pagamento", "Pagamento efetuado com sucesso!")
+        # Obtém o nome e a data de confirmação selecionados pelo usuário
+        nome = self.nome_entry.get()
+        data_confirmacao = datetime.datetime.strptime(self.data_confirmacao_entry.get(), "%d/%m/%Y").date()
+
+        # Efetua o pagamento via PIX na classe ConfirmacaoPresenca
+        if nome:
+            self.confirmacao_presenca.efetuar_pagamento(nome, data_confirmacao)        
+            print("Por favor, selecione um nome.")
+        else:
+            print("Por favor, preencha o campo do nome.")
 
     def reproduzir_playlist(self):
-        # URL da playlist do Spotify
-        playlist_url = "https://open.spotify.com/playlist/37i9dQZF1DX6uhsAfngva3"
+        # Reproduz a playlist no Spotify
+        webbrowser.open(self.playlist.url)
 
-        # Abrir a URL no navegador padrão do usuário
-        webbrowser.open_new_tab(playlist_url)     
+    def iniciar(self):
+        self.janela.mainloop()
 
 
+if __name__ == "__main__":
+# URL da playlist do Spotify
+    playlist_url = "https://open.spotify.com/playlist/37i9dQZF1DWWEJlAGA9gs0"       
 
-root = tk.Tk()
-app = ConfirmacaoPresencaGUI(root)
-root.mainloop()                                       
+    # Cria a instância da classe GUI
+    interface = GUI(playlist_url)
+
+    # Inicia a interface gráfica
+    interface.iniciar()
